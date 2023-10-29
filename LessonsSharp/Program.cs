ec547f8cfe1979b714400c0f093eba2ef9459cb3
Maksim <maxzakrevskiii@gmail.com>
2023-10-29 21:20:51 +0300

    commit number 1, Lesson number 11

---
+++ LessonsSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * == - Равно
+ * != - Не равно
+ * > - больше
+ * < - меньше
+ * >= - больше или равно
+ * <= - меньше или равно
+ */
+
+namespace LessonsSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            int b = 5;
+
+            bool result = a == b;
+
+            Console.WriteLine(result);
+        }
+    }
+}
